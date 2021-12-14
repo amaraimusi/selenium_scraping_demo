8176aef3b31d240090311ad594ef9173ec745799
user <user@host.docker.internal>
2021-12-14 11:18:19 +0900

    アラートの待機 | alert_is_present

---
+++ a001/alert_is_present/script.js
@@ -0,0 +1,14 @@
+
+$(()=>{
+	
+	window.setTimeout(function(){
+		$('#test1').html('猫のしっぽ');
+		
+		let url = 'https://www.youtube.com/embed/Gwkx8DIWxsc';
+		$('#test2').attr('src', url);
+		
+		alert('アラートテスト');
+	}, 2000);
+	
+	
+})
---
+++ shell/01_upload.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz ../ amaraimusi@amaraimusi.sakura.ne.jp:www/selenium_scraping_demo/
+
+echo "------------ Success"
+#cmd /k
